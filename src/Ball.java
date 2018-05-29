4c34a5aa0220f772831984224fd2b2baac86a22c
michael <loo>
2018-05-29 16:05:32 -0400

    Some new things

--- src/Ball.java
+++ src/Ball.java
@@ -1,5 +1,41 @@
 import java.awt.*;
 
 public class Ball {
+    //Red, green, or blue
     private Color color;
+
+    private Point position;
+
+    private int size;
+
+    public Ball(Point position, Color color) {
+        this.position = position;
+
+        this.color = color;
+
+        this.size = 50;
+    }
+
+    //TODO: If a new Ball is created with no color parameter, make the ball a random color (Red, Green, Blue)
+    public Ball(Point position) {
+        this.position = position;
+
+        //Randomize this color!
+        this.color = Color.BLACK;
+
+        this.size = 50;
+    }
+
+    //TODO: Returns a square centered at the Ball's position, with side length = size
+    public Rectangle getBoundingRectangle() {
+        return new Rectangle();
+    }
+
+    //Getters - no need to change
+    public Color getColor() {
+        return color;
+    }
+    public Point getPosition() {
+        return position;
+    }
 }
--- src/BubbleGame.java
+++ src/BubbleGame.java
@@ -1,5 +1,30 @@
-public class BubbleGame {
+import javax.swing.*;
+import java.awt.*;
+
+public class BubbleGame extends JPanel {
+    public BubbleGame(int w, int h) {
+        setSize(w, h);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+    }
+
+    //Main - no need to change
     public static void main(String[] args) {
-        System.out.println("hi its Amelia!");
+        JFrame frame = new JFrame("Bubble Game!");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        int width = 800;
+        int height = 800;
+        frame.setPreferredSize(new Dimension(width, height+24));
+
+        JPanel panel = new BubbleGame(width, height);
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
