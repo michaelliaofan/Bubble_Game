a81cb6d15d39c4291b5a0664ec4644c6a649d215
michael <loo>
2018-06-13 12:43:55 -0400

    BALL GAMERS 1.0 YEET

--- src/BubbleGame.java
+++ src/BubbleGame.java
@@ -11,6 +11,9 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class BubbleGame extends JPanel {
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 800;
+
     private Ball[][] fixedBalls;
     private Ball nextBall;
 
@@ -22,10 +25,10 @@ public class BubbleGame extends JPanel {
 
     private int shotsUntilShift;
 
-    public BubbleGame(int w, int h) {
-        setSize(w, h);
+    public BubbleGame() {
+        setSize(WIDTH, HEIGHT);
 
-        fixedBalls = new Ball[(800-24) / Ball.SIZE][800 / Ball.SIZE];
+        fixedBalls = new Ball[(HEIGHT-24) / Ball.SIZE][WIDTH / Ball.SIZE];
 
         for(int r = 1; r < 4; r++) {
             for(int c = 1; c < fixedBalls[0].length - 1; c++) {
@@ -33,7 +36,7 @@ public class BubbleGame extends JPanel {
             }
         }
 
-        nextBall = new Ball(new Point(800/2, 800-24 - Ball.SIZE), 0, 0);
+        nextBall = new Ball(new Point(WIDTH/2, HEIGHT - 24 - Ball.SIZE), 0, 0);
 
         wasCounted = new boolean[fixedBalls.length][fixedBalls[0].length];
 
@@ -53,7 +56,7 @@ public class BubbleGame extends JPanel {
             @Override
             public void mouseReleased(MouseEvent e) {
                 if(-0.1 <= nextBall.getVelocity() && nextBall.getVelocity() <= 0.1) {
-                    Point initCenter = new Point(w/2, h-24 - Ball.SIZE);
+                    Point initCenter = new Point(WIDTH/2, HEIGHT - 24 - Ball.SIZE);
 
                     double mouseX = e.getX();
                     double mouseY = e.getY();
@@ -97,29 +100,42 @@ public class BubbleGame extends JPanel {
                     for(int c = 0; c < fixedBalls[0].length; c++) {
                         if(fixedBalls[r][c] != null && nextBall != null) {
                             if(nextBall.distanceTo(fixedBalls[r][c]) <= Ball.SIZE) {
-                                int r1 = (int)(nextBall.getCenter().getY()/Ball.SIZE);
-                                int c1 = (int)(nextBall.getCenter().getX()/Ball.SIZE);
+                                //Bouncing ball
+                                if(nextBall instanceof BouncingBall) {
+                                    fixedBalls[r][c] = null;
 
-                                fixedBalls[r1][c1] = new Ball(new Point(c1*Ball.SIZE + Ball.SIZE/2, r1*Ball.SIZE + Ball.SIZE/2), nextBall.getColor(), nextBall.getShadow(), 0, 0);
+                                    ((BouncingBall) nextBall).bounce();
 
-                                nextBall.setCenter(new Point.Double(w/2, h-24 - Ball.SIZE));
-                                nextBall.setVelocity(0, 0);
-                                nextBall.randomizeColor();
-
-                                if(countBalls(fixedBalls[r1][c1].getColor(), r1, c1) > 2) {
-                                    removeBalls();
+                                    if(((BouncingBall) nextBall).getNumBouncesRemaining() <= 0) {
+                                        ((BouncingBall) nextBall).setNumBouncesRemaining(3);
+                                        resetNextBall();
+                                    }
                                 }
 
-                                clearWasCounted();
+                                //Normal ball
+                                else {
+                                    int r1 = (int)(nextBall.getCenter().getY()/Ball.SIZE);
+                                    int c1 = (int)(nextBall.getCenter().getX()/Ball.SIZE);
 
-                                shotsUntilShift--;
+                                    fixedBalls[r1][c1] = new Ball(new Point(c1*Ball.SIZE + Ball.SIZE/2, r1*Ball.SIZE + Ball.SIZE/2), nextBall.getColor(), nextBall.getShadow(), 0, 0);
 
-                                if(shotsUntilShift == 0) {
-                                    shotsUntilShift = 5;
-                                    shiftBalls();
+                                    resetNextBall();
+
+                                    if(countBalls(fixedBalls[r1][c1].getColor(), r1, c1) > 2) {
+                                        removeBalls();
+                                    }
+
+                                    clearWasCounted();
+
+                                    shotsUntilShift--;
+
+                                    if(shotsUntilShift == 0) {
+                                        shotsUntilShift = 5;
+                                        shiftBalls();
+                                    }
+
+                                    break outer;
                                 }
-
-                                break outer;
                             }
                         }
                     }
@@ -259,6 +275,12 @@ public class BubbleGame extends JPanel {
         from = null;
     }
 
+    private void resetNextBall() {
+        nextBall.setCenter(new Point.Double(WIDTH/2, HEIGHT-24 - Ball.SIZE));
+        nextBall.setVelocity(0, 0);
+        nextBall.randomizeColor();
+    }
+
     private boolean win() { //edit this is the win condition
         for(int r = 0; r<fixedBalls.length; r++){
             for(int c = 0; c<fixedBalls[0].length; c++){
@@ -285,6 +307,7 @@ public class BubbleGame extends JPanel {
         //g2.setColor(new Color (255, 176, 221));
         g2.fillRect(800, 0, 200, 800);
 
+        //g2.
         g2.setColor(new Color(108, 204, 211));
         g2.fillRect(0, 0, 1000, 50);
         g2.fillRect(0, 0, 50, 800);
@@ -358,11 +381,9 @@ public class BubbleGame extends JPanel {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Bubble Game!");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        int width = 1000;
-        int height = 800;
-        frame.setPreferredSize(new Dimension(width, height+24));
+        frame.setPreferredSize(new Dimension(WIDTH, HEIGHT+24));
 
-        JPanel panel = new BubbleGame(width, height);
+        JPanel panel = new BubbleGame();
         panel.setFocusable(true);
         panel.grabFocus();
 
